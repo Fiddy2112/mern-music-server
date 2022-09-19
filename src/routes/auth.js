9dc643bbeb81213701fe03f97a8cac74c6f5902f
Fiddy <85234199+Felixx2112@users.noreply.github.com>
2022-09-19 20:52:50 +0700

    add apis get all user in auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,5 +3,6 @@ const authController = require("../controllers/AuthController");
 const router = express.Router();
 
 router.get("/login", authController.login);
+router.get("/getUsers", authController.getUsers);
 
 module.exports = router;
